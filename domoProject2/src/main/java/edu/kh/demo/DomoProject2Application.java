5113b484abfee1a668c22b6a1ed38641c7fffbd9
miae094 <miae094@gmail.com>
2025-04-15 18:01:41 +0900

    타임리프 기초

---
+++ domoProject2/src/main/java/edu/kh/demo/DomoProject2Application.java
@@ -0,0 +1,13 @@
+package edu.kh.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomoProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomoProject2Application.class, args);
+	}
+
+}
