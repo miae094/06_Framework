7367c4a838e7443bdab1ce381289edc6a56f8f44
miae094 <miae094@gmail.com>
2025-04-15 15:32:00 +0900

    스프링부트 기본 사용법

--- demoProject/src/main/java/edu/kh/demo/controller/ParameterController.java
+++ demoProject/src/main/java/edu/kh/demo/controller/ParameterController.java
@@ -1,10 +1,18 @@
 package edu.kh.demo.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import edu.kh.demo.DemoProjectApplication;
+import edu.kh.demo.model.dto.MemberDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +20,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("param")		// /param으로 시작하는 요청을 현재 컨트롤러로 매핑
 @Slf4j	// log를 이용한 메시지 콘솔창에 출력할 때 사용(Lombok 제공)
 public class ParameterController {
+
+    private final DemoProjectApplication demoProjectApplication;
+
+    ParameterController(DemoProjectApplication demoProjectApplication) {
+        this.demoProjectApplication = demoProjectApplication;
+    }
 	
 	@GetMapping("main")		// /param/main GET 방식 요청 매핑
 	public String paramMain() {
@@ -54,4 +68,94 @@ public class ParameterController {
 		
 		return "redirect:/param/main";
 	}
+	
+	/* 2. @RequestParam 어노테이션 - 낱개 파라미터 얻어오기
+	 * - request 객체를 이용한 파라미터 전달 어노테이션
+	 * - 매개변수 앞에 해당 어노테이션을 작성하면, 매개변수에 값이 주입됨.
+	 * - 주입되는 데이터는 매개변수의 타입에 맞게 형변환이 자동으로 수행됨.
+	 * 
+	 * [작성법]
+	 * @RequestParam("key") 자료형 매개변수명
+	 * 
+	 * [속성 추가 작성법]
+	 * @RequestParam(value = "key", required=false, defaultValue="1")
+	 * 
+	 * value : 전달받은 input 태그의 name 속성값(파라미터 key)
+	 * required : 입력된 name 속성값 파라미터 필수 여부 지정(기본값 true)
+	 * -> required=true 인 파라미터가 존재하지 않는다면 400(Bed Request) 에러 발생
+	 * -> "" (빈문자열) 일 때는 에러 발생 X(파라미터가 존재하지 않는것이 아니라 name 속성값="" 로 넘어오기 때문에.)
+	 * 
+	 * defaultValue : 파라미터 중 일치하는 name 속성값이 없을 경우에 대입할 값 지정.
+	 * -> required=false 인 경우 사용
+	 * 
+	 * */
+	
+	@PostMapping("test2")		// /param/test2 POST 방식 요청 매핑하는 메서드다
+	public String paramTest2(@RequestParam("title") String title,
+							@RequestParam("writer") String writer,
+							@RequestParam("price") int price, // 형변환이 필요한 경우에는 파라미터 value값 필수 작성 되어있어야 함.
+							@RequestParam("publisher") String publisher) {
+		
+		log.debug("title : " + title);
+		log.debug("writer : " + writer);
+		log.debug("price : " + price);
+		log.debug("publisher : " + publisher);
+		
+		return "redirect:/param/main";
+	}
+	
+	
+	// 3. @RequestParam 여러개 파라미터
+	// - 같은 name 속성값을 가진 파라미터 얻어오기 (String[], List<String> )
+	// - 제출된 파라미터 한번에 묶어서 얻어오기 (Map<String, Object>)
+	
+	@PostMapping("test3")
+	public String paramTest3(@RequestParam("color") String[] colorArr,
+							@RequestParam("fruit") List<String> fruitList,
+							@RequestParam Map<String, Object> paramMap) {
+		
+		log.debug("colorArr : " + Arrays.toString(colorArr));
+		
+		log.debug("fruitList : " + fruitList);
+		
+		// @RequestParam Map<String, Object> paramMap
+		// -> 제출된 모든 파라미터가 Map 에 저장된다
+		// -> 같은 name 속성을 가진 파라미터는 String[], List 로 저장되는게 아니라
+		//	  첫번째로 제출된 value 값이 저장됨
+		log.debug("paramMap : " + paramMap);
+		// paramMap : {color=Red, fruit=Apple, productName=키키오렌지, expirationDate=2025-04-25}
+		
+		
+		return "redirect:/param/main";
+	}
+	
+	// 4. @ModelAttribute 를 이용한 파라미터 얻어오기
+	// @ModelAttribute
+	// - DTO(또는 VO)와 같이 사용하는 어노테이션
+	
+	// 전달받은 파라미터의 name 속성값이 함께 사용되는 DTO의 필드명과 같다면
+	// 자동으로 setter 를 호출해서 필드에 값을 저장
+	
+	// ** 주의사항 **
+	// - DTO 에 기본생성자가 필수로 존재해야 한다.
+	// - DTO 에 setter 가 필수로 존재해야 한다.
+	
+	// @ModelAttribute 를 이용해 값이 필드에 세팅된 객체를
+	// "커맨드 객체" 라고 부른다
+	
+	// @ModelAttribute 생략가능!
+	// @ModelAttribute MemberDTO inputMember => MemberDTO inputMember 이렇게 써도 된다는 뜻
+	@PostMapping("test4")
+	public String paramTest4(/*@ModelAttribute*/ MemberDTO inputMember) {
+		
+		log.debug("inputMember : " + inputMember);
+		
+		return "redirect:main";	// 상대경로 작성법
+		// == "redirect:/param/main"
+		//(현재 위치가 중요!
+		// -> 현재 경로의 가장 마지막 부분을 redirect:이주소 로 변경하기 때문!)
+		
+		// 만약 "redirect"test/result" 라면 -> /param/test/result 가되는것!
+		}
+	
 }
