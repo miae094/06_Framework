d4427fc82624c0e286ce90a3ee61b84ca532ba1b
miae094 <miae094@gmail.com>
2025-04-22 17:43:51 +0900

    로그인 및 메일발송

--- boardProject-boot/src/main/java/edu/kh/project/member/controller/MemberController.java
+++ boardProject-boot/src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -3,14 +3,20 @@ package edu.kh.project.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.member.model.dto.Member;
 import edu.kh.project.member.model.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @SessionAttributes({"loginMember"})
@@ -41,7 +47,14 @@ public class MemberController {
 	 * @return
 	 */
 	@PostMapping("login")
-	public String login(@ModelAttribute Member inputMember, RedirectAttributes ra, Model model) {
+	public String login(@ModelAttribute Member inputMember, RedirectAttributes ra,
+			Model model, @RequestParam(value="saveId", required = false) String saveId,
+			HttpServletResponse resp) {
+		
+		// 체크박스
+		// - 체크가 된 경우 	: "on"
+		// - 체크가 안 된 경우 	: null
+		
 		
 		// 로그인 서비스 호출
 		Member loginMember = service.login(inputMember);
@@ -57,13 +70,66 @@ public class MemberController {
 			// 2단계 : 클래서 위에 @SessionAttribute 어노테이셔 작성하여
 			// 			session scope로 loginMember를 이동
 			
+			// ******************** Cookie ***********************
+			//로그인 시 작성한 이메일 저장(쿠키에)
+			
+			// 쿠키 객체 생성( K : V )
+			Cookie  cookie = new Cookie("saveId", loginMember.getMemberEmail());
+			// saveId = user01@kh.or.kr
+			log.debug("cookie : " + cookie);
+			log.debug("loginMember.getMemberEmail() : " + loginMember.getMemberEmail());
+			//클라이언트가 어떤 요청을 할 때 쿠키를 첨부할지 지정
+			// ex) ":/" : IP 또는 도메인 localhost
+			//			--> 메인페이지 + 그 하위주소 
+		
+			cookie.setPath("/");
+			
+			// 쿠키의 만료기간 지정
+			if(saveId != null) { // 아이디 저장 체크박스 체크했을 때
+				cookie.setMaxAge(60 * 60 * 24 * 30);// 30일 초 (단우로 지정)
+				
+			} else { // 체크 안했을 때
+				cookie.setMaxAge(0); 	// 0초 (클라이언트 쿠키 바로 삭제
+				
+			}
+			
+			// 응답 객체에 쿠키 추가 -> 클라이언트에게 전달
+			resp.addCookie(cookie);
+			
 		}
-		
-		
-		
-		
 		
 		
 		return "redirect:/";		
 	}
+	
+	/** 로그아웃 : session에 저장된 로그인 된 회원 정보를 없앰
+	 * @param essionStatus :  @SesstionAttributes로 지정된 특정 속성을 세션에서 제거 가능
+	 * @return
+	 */
+	@GetMapping("logout")
+	public String logout(SessionStatus status) {
+		status.setComplete(); // 세션을 완료시킴(= @SessionAttribute로 등록된 세션 제거
+		return "redirect:/";
+		
+	}
+	
+	/** 회원 가입 페이지로 이동
+	 * @return
+	 */
+	@GetMapping("signup")
+	public String signupPage() {
+		return "member/signup";
+	}
+	
+	/** 이메일 중복검사(비동기 요청)
+	 * @return
+	 */
+	@ResponseBody	// 응답/ 본문(fetch)으로 돌려보냄
+	@GetMapping("checkEmail")
+	public int checkEmail(@RequestParam("memberEmail") String memberEmail) {
+		return service.checkEmail(memberEmail);
+	}
+	
+	
+	
 }
