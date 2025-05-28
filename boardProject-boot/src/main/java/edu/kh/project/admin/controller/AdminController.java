b32bcc85a966f08b09ae334f7fb02cb844d9f74e
miae094 <miae094@gmail.com>
2025-05-28 10:36:21 +0900

    관리자 메뉴 연결/차트/로그아웃

--- boardProject-boot/src/main/java/edu/kh/project/admin/controller/AdminController.java
+++ boardProject-boot/src/main/java/edu/kh/project/admin/controller/AdminController.java
@@ -1,7 +1,10 @@
 package edu.kh.project.admin.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import edu.kh.project.admin.model.service.AdminService;
 import edu.kh.project.member.model.dto.Member;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,8 +40,26 @@ public class AdminController {
 		return loginMember;
 	}
 	
-	
-	
+	/** 관리자 로그아웃
+	 * @return
+	 */
+	@GetMapping("logout")
+	public ResponseEntity<String> logout(HttpSession session){
+		// ResponseEntity
+		// Spring 에서 제공하는 HTTP 응답 데이터를
+		// 커스터마이징 할 수 있도록 지원하는 클래스
+		// -> HTTP 상태코드, 헤더, 응답 본문(body)을 모두 설정 가능
+		
+		try {
+			session.invalidate(); // 세션 무효화 처리
+			return ResponseEntity.status(HttpStatus.OK) // 200
+					.body("로그아웃이 완료되었습니다."); 
+		} catch (Exception e) {
+			// 세션 무효화 중 예외 발생한 경우
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR) // 500
+					.body("로그아웃 중 예외 발생 : " + e.getMessage()); 
+		}
+	}
 	
 	
 }
